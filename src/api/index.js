952a7097a47124bf431e713b2e07a64d49dad6df
Oluwapelumi <fafyeepelumi04@gmail.com>
2022-03-28 04:21:30 -0700

    latest commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
 // const URL = 'https://asos2.p.rapidapi.com/categories/list';
 
@@ -10,35 +10,35 @@ import axios from 'axios';
 //     }
 // };
 
-const URL = 'https://asos2.p.rapidapi.com/products/v2/list'
+// const URL = 'https://asos2.p.rapidapi.com/products/v2/list'
 
-const options = {
-    params: {
-        store: 'US',
-        offset: '0',
-        categoryId: '4209',
-        limit: '48',
-        country: 'US',
-        sort: 'freshness',
-        currency: 'USD',
-        sizeSchema: 'US',
-        lang: 'en-US'
-    },
-    headers: {
-        'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
-        'X-RapidAPI-Key': '50e5875042mshc342f1fcf2d5632p14862ajsnc51b3f2e63c7'
-    }
-};
+// const options = {
+//     params: {
+//         store: 'US',
+//         offset: '0',
+//         categoryId: '4209',
+//         limit: '48',
+//         country: 'US',
+//         sort: 'freshness',
+//         currency: 'USD',
+//         sizeSchema: 'US',
+//         lang: 'en-US'
+//     },
+//     headers: {
+//         'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
+//         'X-RapidAPI-Key': '50e5875042mshc342f1fcf2d5632p14862ajsnc51b3f2e63c7'
+//     }
+// };
 
 
 
-export const getData = async () => {
-    try {
-        const { data: { data } } = await axios.get(URL, options);
+// export const getData = async () => {
+//     try {
+//         const { data: { data } } = await axios.get(URL, options);
 
-        return data;
+//         return data;
 
-    } catch (error) {
-        console.log(error)
-    }
-}
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,25 +1,34 @@
 import React, { useEffect, useState } from 'react';
-import { getData } from '../api';
+// import { getData } from '../api';
 
 const Products = () => {
 
     const [list, setList] = useState([]);
 
     useEffect(() => {
-        getData()
-            .then((data) => {
-                // console.log(data);
+        // getData()
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Host': 'asos2.p.rapidapi.com',
+                'X-RapidAPI-Key': '50e5875042mshc342f1fcf2d5632p14862ajsnc51b3f2e63c7'
+            }
+        };
 
-                setList(data);
-            })
+        fetch('https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=8&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US', options)
+            .then(response => response.json())
+            .then(data => setList(data.products))
+            .catch(err => console.error(err));
     }, [])
 
     return (
         <div>
             {list.map((item) => {
                 return (
-                    <div>
-                        <img src={item.products.imageUrl} alt />
+                    <div key={item.id}>
+                        <img src={item.imageUrl} alt='image' />
+                        <p>{item.name}</p>
+                        <p>${item.price.current.value}</p>
                     </div>
                 );
             })}
